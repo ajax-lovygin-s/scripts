d275d1cbbd3bae06f572c818887da13e257aae62
Sergii Lovygin <lovygin.s@ajax.systems>
2022-10-24 14:13:09 +0300

    add generation of file with random data

--- make-network-traffic.py
+++ make-network-traffic.py
@@ -1,15 +1,35 @@
 from pathlib import Path
+from random import choice
+from string import ascii_uppercase, digits
 
 from pexpect import spawn, EOF
 
 
-def generate_file(filename: Path) -> None:
+def generate_random_string(size: int = 1024, allowed_chars: str = ascii_uppercase + digits) -> str:
+    return ''.join(choice(allowed_chars) for _ in range(size))
+
+
+def generate_file(path: Path, size: int, line_length: int = 1024) -> None:
+    with open(path, 'w') as file:
+        lines_count, extra_line_size = divmod(size, line_length)
+        for _ in range(lines_count):
+            generated_line = generate_random_string(line_length) + "\n"
+            file.write(generated_line)
+
+        if extra_line_size:
+            generated_line = generate_random_string(extra_line_size) + "\n"
+            file.write(generated_line)
+
+
+def generate_content_file(filename: Path) -> None:
+    """Generate small file (~5Mb) with trash content."""
     filename.parent.mkdir(parents=True, exist_ok=True)
     if filename.exists():
-        print(f"file is already exist and will be truncated")
+        print(f'file ({filename}) is already exist and will be truncated')
 
-    with open(filename, "w") as file:
-        file.write('test')
+    file_size = 5*1024*1024
+    generate_file(path=filename, size=file_size, line_length=120)
+    print(f'file ({filename}) generated: size = {filename.stat().st_size}')
 
 
 def send_file(source: Path, user: str, host: str, destination: Path, password: str) -> None:
@@ -25,8 +45,8 @@ def send_file(source: Path, user: str, host: str, destination: Path, password: s
 
 
 def main() -> None:
-    source = Path(__file__).parent / 'cache' / 'to-device'
-    generate_file(source)
+    source = Path(__file__).parent / '.cache' / 'to-device'
+    generate_content_file(source)
 
     print('starting do network activity')
     try:
